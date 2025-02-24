8d54333af65cd004febf6e43150f0f71cd3dc01b
Billy <billyLin1954@gmail.com>
2025-02-23 21:38:32 -0500

    discourse

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import express,{json} from 'express';
 import  {MongoClient}  from 'mongodb';
+import { hash, compare } from 'bcryptjs';
+
+
 const uri = "mongodb+srv://billylin1954:Cracknut4@cluster0.rrsqc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const client = new MongoClient(uri);
 const app = express();
-//const { readFile } = promises;
-
-// Serve static files from the "public" directory
+  
 app.use(express.static('public'));
 
 // Define a route for the login function
@@ -17,12 +18,59 @@ const PORT = 3005;
 // Middleware to parse JSON data sent from the frontend
 app.use(json());
 // Route to receive data from the frontend
+app.get('/user', (req, res) => {
+  res.json(users)
+})
+app.get('/user/login', (req, res) => {
+  res.json(users)
+})
+const users = [{"name":"1234","password":"$2b$10$RLhBdtSFsA/M97tmqZdbCeKqYS15crG1/SlNmPHOvWPp5Y3JaRnDy"}]
+app.post('/user', async (req, res) => {
+  try {
+    const user_data=req.body
+    const name = user_data.user;
+    const password=user_data.password
+  
+    // Logging uploading file
+    console.log(name);
+    console.log(password)
+    console.log("working")
+    const hashedPassword = await hash(password, 10)
+    console.log(hashedPassword)
+    const user = { name: name, password: hashedPassword }
+    users.push(user)
+    console.log(user)
+    res.status(201).send()
+  } catch {
+    res.status(500).send()
+  }
+})
+app.post('/user/login', async (req, res) => {
+  const user = users.find(user => user.name === req.body.name)
+  console.log("working")
+  if (user == null) {
+    console.log("not working")
+    return res.status(400).send('Cannot find user')
+  }
+  try {
+    if(await compare(req.body.password, user.password)) {
+      console.log("working pass")
+      res.json({ message: 'pass' });
+    } else {
+      res.send('Not Allowed')
+    }
+  } catch {
+ //   res.status(500).send()
+  }
+})
+
 app.post('/api/data', (req, res) => {
+  console.log("work")
   async function run() {
    try {
      await client.connect();
      console.log("Connected successfully to MongoDB");
-  
+     
      const database = client.db('billCo');
      const collection = database.collection('stuff');
   
@@ -30,12 +78,10 @@ app.post('/api/data', (req, res) => {
      const password = receivedData.password
      const type="customer";
      // Insert the document into MongoDB
-     if(password>0){
-      
-     }
+     
      await collection.insertOne({ user, password ,type});
      console.log("Data inserted:", { user, password });
-  
+     console.log("working")
   
    } catch (error) {
      console.error("Error:", error);
@@ -48,11 +94,34 @@ app.post('/api/data', (req, res) => {
   console.log('Data received from frontend:', receivedData);
 
   
-  
   run()
  
-  res.json({ message: 'Data received successfully', data: receivedData });
+  res.json({ message: 'data received successfully', data: receivedData });
 });
+app.get('/api/data', async function (req, res) {
+  
+  try {
+    await client.connect();
+    console.log("Connected successfully to MongoDB");
+
+    const database = client.db('billCo');
+    const collection = database.collection('stuff');
+    
+   
+    const result = await collection.find().limit(400).toArray();
+    console.log(result)
+     
+    res.send({ response:result} )
+  } catch (error) {
+    console.error("Error:", error);
+    
+  } finally {
+    await client.close();
+    console.log("Connection closed.");
+  }
+});
+
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
--- public/app.js
+++ public/app.js
@@ -3,12 +3,19 @@ let  datatosend = {
     user:"",
     password:""
   };
-  console.log(document.getElementById("username").innerHTML)
+let user_data={
+  user:"",
+  password:""
+}
   function run(){
       datatosend.user=document.getElementById("username").value
       console.log( datatosend.user)
-      datatosend.password=document.getElementById("password").value
+      datatosend.password="12"
       console.log( datatosend.password)
+      var ul = document.getElementById("feed");
+      var li = document.createElement("li");
+      li.appendChild(document.createTextNode(datatosend.user+":"+datatosend.password));
+      ul.appendChild(li);
       fetch('/api/data', {
         method: 'POST',
         headers: {
@@ -24,5 +31,94 @@ let  datatosend = {
           console.error('Error sending data:', error);
         });
       
+    
+      }
+      let chat=[]
+      fetch('http://localhost:3005/api/data')
+    .then((response) => response.json())
+    .then((result) => {
+      if (Array.isArray(result.response)) {
+        console.log(result.response)
+        chat=  result.response
+        console.log(chat[0].user)
+     
+      }
+    })
+    .catch((error) => {
+      console.error('Error fetching data:', error);
+    });
+    function load(item){
+      var ul = document.getElementById("feed");
+      var li = document.createElement("li");
+      li.appendChild(document.createTextNode(item));
+      ul.appendChild(li);
     }
+function load_one(){
+console.log("working")
+fetch('http://localhost:3005/api/data')
+.then((response) => response.json())
+.then((result) => {
+  if (Array.isArray(result.response)) {
+    console.log(result.response)
+    chat=  result.response
+    console.log(chat[0].user)
+ 
+  }
+})
+.catch((error) => {
+  console.error('Error fetching data:', error);
+});
+ for(var i=0;i<chat.length;i++){
+  var ul = document.getElementById("feed");
+  var li = document.createElement("li");
+  li.appendChild(document.createTextNode(chat[i].user+":"+chat[i].password));
+  ul.appendChild(li);
+ }
+}
   
+function sign_up(){
+  user_data.user=document.getElementById("user").value
+  
+  user_data.password=document.getElementById("password").value
+  console.log( user_data.password)
+  
+  fetch('http://localhost:3005/user', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(user_data),
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Response from server:', data);
+    })
+    .catch(error => {
+      console.error('Error sending data:', error);
+    });
+  }
+  let compare_password={
+     user:"",
+  password:""
+  }
+  function login(){
+   compare_password.name=document.getElementById("compare_user").value
+    
+    compare_password.password=document.getElementById("compare_password").value
+    console.log( compare_password.password)
+    
+    fetch('http://localhost:3005/user/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(compare_password),
+    })
+      .then(response => response.json())
+      .then(message => {
+        console.log('Response from server:', message);
+      })
+      .catch(error => {
+        console.error('Error sending data:', error);
+      });
+    }
